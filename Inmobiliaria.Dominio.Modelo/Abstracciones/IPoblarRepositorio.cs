202ea20c1f35d67aa6c9af79720ad47e5e09dae1
Bryan <bral_9210@hotmail.com>
2022-01-25 22:31:31 -0500

    Crear metodo generico

---
+++ Inmobiliaria.Dominio.Modelo/Abstracciones/IPoblarRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inmobiliaria.Dominio.Modelo.Abstracciones
+{
+    public interface IPoblarRepositorio<TEntity> where TEntity : class
+    {
+        List<TEntity> ObtenerTodos();
+
+        List<TEntity> ObtenerDepentientes(int id);
+    }
+}
---
+++ Inmobiliaria.Infraestructura.AccesoDatos/Repositorio/PoblarRepositorio.cs
@@ -0,0 +1,51 @@
+﻿using Inmobiliaria.Dominio.Modelo.Abstracciones;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inmobiliaria.Infraestructura.AccesoDatos.Repositorio
+{
+    public class PoblarRepositorio<TEntity> : IDisposable, IPoblarRepositorio<TEntity> where TEntity : class
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        public List<TEntity> ObtenerDepentientes(int id)
+        {
+            try
+            {
+                using (var context = new INMOBILIARIAEntities1())
+                {
+                    return context.Set<TEntity>().ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("No se puede recuperar el registro" + ex.Message);
+            }
+        }
+
+        public List<TEntity> ObtenerTodos()
+        {
+            try
+            {
+                using (var context = new INMOBILIARIAEntities1())
+                {
+                    return context.Set<TEntity>().ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("No se puede recuperar los registros" + ex.Message);
+            }
+        }
+    }
+}
